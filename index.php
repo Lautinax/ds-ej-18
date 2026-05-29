1e6f15ac3362f3936b155e6ec8c66622b7604825
Lautinax <lautaroferraro214@gmail.com>
2026-05-29 20:46:57 -0300

    git commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'modelo/motor.php';
+require_once 'modelo/auto.php';
+
+
+$m = new Motor();
+$m->Tipo = '3.0';
+$m->Potencia = '300 HP';
+$m->Combustible = 'GNC';
+
+
+$a = new Auto();
+$a->Marca = 'Peugeot';
+$a->Modelo = 'Planchado';
+$a->Color = 'Verde';
+$a->Motor = $m;
+
+$a->MostrarAuto();
